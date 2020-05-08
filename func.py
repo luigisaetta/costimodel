247928517b8e18c18ef4c14e1a67c0cb958cd083
Luigi Saetta <lsaetta@MBP-di-Luigi.local>
2020-05-08 15:55:08 +0200

    added check on num cols

--- func.py
+++ func.py
@@ -10,7 +10,7 @@ import scorefn
 model = scorefn.load_model()
 
 def handler(ctx, data: io.BytesIO=None):
-    logging.getLogger().info("Costi-model: vers. 1.1")
+    logging.getLogger().info("Costi-model: vers. 1.2")
     
     try:
         input = json.loads(data.getvalue())['input']
@@ -19,11 +19,18 @@ def handler(ctx, data: io.BytesIO=None):
         input_json = json.dumps(input)
         logging.getLogger().info("Costi-model: Invoked with input %s", input_json)
 
+        # check num columns
+        num_cols = len(input[0])
+
+        # attende 12 colonne per vettore
+        assert num_cols == 12
+
         prediction = scorefn.predict(model, input)
+
+        logging.getLogger().info("Costi-model: prediction %s", json.dumps(prediction))
+
     except (Exception, ValueError) as ex:
         logging.getLogger().error("%s", str(ex))
-
-    logging.getLogger().info("Costi-model: prediction %s", json.dumps(prediction))
 
     return response.Response(
         ctx, response_data=json.dumps(prediction),
