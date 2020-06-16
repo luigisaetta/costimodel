5d73c00e66cf0d00a69cb0001200f16a4704ae14
Luigi Saetta <lsaetta@MBP-di-Luigi.local>
2020-06-16 20:54:03 +0200

    added content

--- batch/func.py
+++ batch/func.py
@@ -30,39 +30,42 @@ def handler(ctx, data: io.BytesIO=None):
         resourceName = body["data"]["resourceName"]
         eventType = body["eventType"]
         
-        logging.info('***eventType: ' + eventType + ', resourceName: ' + resourceName)
+        # solo i file csv sono elaborati
+        if "csv" in resourceName:
 
-        # il nome del file e' in resourceName
-        namespace = os.environ.get("OCI_NAMESPACE")        
-        bucket_name = os.environ.get("OCI_BUCKET")
+            logging.info('***eventType: ' + eventType + ', resourceName: ' + resourceName)
 
-        obj_file = client.get_object(namespace, bucket_name, resourceName)
+            # il nome del file e' in resourceName
+            namespace = os.environ.get("OCI_NAMESPACE")        
+            bucket_name = os.environ.get("OCI_BUCKET")
 
-        content = obj_file.data.content.decode('UTF-8')
+            obj_file = client.get_object(namespace, bucket_name, resourceName)
 
-        # preparo il nome del file di report
-        base_name = resourceName.split('.')[0]
-        report_name = base_name + "_report.txt"
+            content = obj_file.data.content.decode('UTF-8')
 
-        # uso pandas per il parsing del csv
-        df = pd.read_csv(StringIO(content))
+            # preparo il nome del file di report
+            base_name = resourceName.split('.')[0]
+            report_name = base_name + "_report.txt"
 
-        # elimino le colonne anno e mese che non sono usate dal modello
-        df = df.drop('anno', axis=1)
-        df = df.drop('mese', axis=1)
+            # uso pandas per il parsing del csv
+            df = pd.read_csv(StringIO(content))
 
-        # in questo modo ho una lista di liste
-        lista = df.values
+            # elimino le colonne anno e mese che non sono usate dal modello
+            df = df.drop('anno', axis=1)
+            df = df.drop('mese', axis=1)
 
-        for vet in lista:
-            # vet è un vettore di 12 elementi
-            if vet.shape[0] == 12:
-                logging.info('riga: ' + str(vet))        
+            # in questo modo ho una lista di liste
+            lista = df.values
+
+            for vet in lista:
+                # vet è un vettore di 12 elementi
+                if vet.shape[0] == 12:
+                    logging.info('riga: ' + str(vet))        
         
-        my_data = b'test report...'
+            my_data = b'test report...'
 
-        client.put_object(namespace, bucket_name, report_name, my_data)
-        
+            client.put_object(namespace, bucket_name, report_name, my_data)
+
     except Exception as ex:
         logging.getLogger().error("%s", str(ex))
     
