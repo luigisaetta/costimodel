fc30127189544fb635892cd7276ea5cb09be59e0
Luigi Saetta <lsaetta@MBP-di-Luigi.local>
2020-06-17 08:40:00 +0200

    encoding, some small changes

--- batch/func.py
+++ batch/func.py
@@ -19,6 +19,7 @@ sys.path.append('/function')
 
 # config
 # numero di colonne attese per vettore input
+ENCODING = 'UTF-8'
 NUM_COLS = 12
 model = scorefn.load_model()
 
@@ -43,14 +44,13 @@ def handler(ctx, data: io.BytesIO=None):
 
             # il nome del file e' in resourceName
             # compartment è il compartment della function ML da invocare
-            compartment = os.environ.get("OCI_COMPARTMENT")
             namespace = os.environ.get("OCI_NAMESPACE")        
             bucket_name = os.environ.get("OCI_BUCKET")
 
             # legge il contenuto del file
             obj_file = client.get_object(namespace, bucket_name, resourceName)
 
-            content = obj_file.data.content.decode('UTF-8')
+            content = obj_file.data.content.decode(ENCODING)
 
             # preparo il nome del file di report
             base_name = resourceName.split('.')[0]
@@ -66,7 +66,7 @@ def handler(ctx, data: io.BytesIO=None):
             # in questo modo ho una lista di liste
             lista = df.values
 
-            report = "Report relativo al file: " + resourceName + "\n"
+            report = "Report relativo al file: " + resourceName + "\n\n"
 
             # invoco la predizione
             logging.getLogger().info("Costi-model: Invoked...")
@@ -74,15 +74,15 @@ def handler(ctx, data: io.BytesIO=None):
             prediction = scorefn.predict(model, lista)
 
             logging.getLogger().info("Costi-model: prediction %s", json.dumps(prediction))
-            logging.getLogger().info("Type: prediction %s", type(prediction))
 
-            # prediction è un dict
+            # prediction è un dictionary, estraggo la lista delle predizioni
             vet_prediction = prediction['prediction']
 
+            # preparo il report
             index = 0
             for vet in lista:
                 # vet è un vettore di 12 elementi
-                if vet.shape[0] == 12:
+                if vet.shape[0] == NUM_COLS:
                     logging.info('riga: ' + str(vet) + ", " + str(vet_prediction[index]))
 
                     report = report + "input: " + str(vet) + ", predizione: " + str(vet_prediction[index]) + "\n"
@@ -90,17 +90,17 @@ def handler(ctx, data: io.BytesIO=None):
 
 
             # produce il report
-            my_data = report.encode('UTF-8')
+            my_data = report.encode(ENCODING)
 
             client.put_object(namespace, bucket_name, report_name, my_data, content_type='text/csv')
 
     except Exception as ex:
         logging.getLogger().error("%s", str(ex))
     
-    prediction = {}
-    prediction['response'] = 'OK'
+    result = {}
+    result['response'] = 'OK'
 
     return response.Response(
-        ctx, response_data=json.dumps(prediction),
+        ctx, response_data=json.dumps(result),
         headers={"Content-Type": "application/json"}
     )
